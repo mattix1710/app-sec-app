a1c9639cee4360b7304cc1056cc7aa2214b6f921
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2023-11-28 20:48:47 +0100

    🔧 Changed model name from MERGE login

--- app/models.py
+++ app/models.py
@@ -3,16 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 from . import db
 
-class users(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(72), nullable=False)
-    is_active = db.Column(db.Boolean, nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False)
-
 # model class created ONLY for testing purposes
 # TODO: delete before finishing the project
 class Photo(db.Model):
