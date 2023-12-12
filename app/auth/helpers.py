d9b1c91df5ea7e2b3960a33d84475a28840555b6
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2023-12-12 15:27:17 +0100

    ✨ Added session checker method

--- app/auth/helpers.py
+++ app/auth/helpers.py
@@ -12,3 +12,33 @@ def hash_the_pass(passwd):
     
     # returning the hash in decoded version from bytes to string
     return hashed_pw.decode('utf-8')
+
+#############################################
+# TIME DELTA SECONDS
+DELTA_SECS = 3600
+
+def check_session():
+    '''
+        Checks whether the session is ok or wrong
+    '''
+    if 'session' in session:
+        session_data = Session.query.where(Session.id == session['session']).scalar()
+        # if session with this ID exists
+        if session_data != None:
+            # if session didn't expire
+            if session_data.timestamp + timedelta(0, DELTA_SECS) > datetime.now():
+                return True
+    # otherwise - pop the data from cookie and return False
+    session.pop('session')
+    return False
+
+def set_session(login):
+    '''
+        Sets new session based on given credentials
+    '''
+    new_session = Session(uid = User.query.where(User.username == login).scalar().id)
+    db.session.add(new_session)
+    
+    session['session'] = Session(uid = User.query.where(User.username == login).scalar().id).id
+    
+    db.session.commit()
