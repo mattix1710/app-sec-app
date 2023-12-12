fdee6ba3e424f24aaa7e611a6cec38c0f29ce404
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2023-12-12 10:44:53 +0100

    🗃️ Updated database structure

--- app/models.py
+++ app/models.py
@@ -17,12 +17,22 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, server_default=db.FetchedValue())
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(72), nullable=False)
     is_active = db.Column(db.Boolean, nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, server_default=db.FetchedValue())
+    last_logged = db.Column(db.Date, nullable=False, server_default=db.FetchedValue())
 
     # INFO: only for DEBUG purposes
     def get_user_data(self):
-        print("User - {}\nemail: {}\npass_hash: {}".format(username, email, password))
+        print("User - {}\nemail: {}\npass_hash: {}".format(self.username, self.email, self.password))
+        
+class Session(db.Model):
+    __tablename__ = 'sessions'
+
+    id = db.Column(db.Uuid, primary_key=True, server_default=db.FetchedValue())
+    uid = db.Column(db.ForeignKey('users.id'), nullable=False, server_default=db.FetchedValue())
+    timestamp = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+
+    user = db.relationship('User', primaryjoin='Session.uid == User.id', backref='sessions')
---
+++ settings/droplet_db.sql
@@ -0,0 +1,36 @@
+CREATE TABLE "users"(
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(255)    NOT NULL,
+    "password" VARCHAR(72)  NOT NULL,
+    "is_active" BOOLEAN     NOT NULL,
+    "is_admin" BOOLEAN      NOT NULL DEFAULT FALSE,
+    "last_logged" date      NOT NULL DEFAULT NOW(),
+
+    UNIQUE("username"),
+    UNIQUE("email")
+);
+
+CREATE TABLE "user_personal_data"(
+    "user_id" SERIAL PRIMARY KEY REFERENCES users(id),
+    "PESEL" CHAR(11)        UNIQUE KEY,
+    "first_name" VARCHAR(100),
+    "last_name" VARCHAR(100),
+    "blood_type" VARCHAR(5)
+);
+
+CREATE TABLE "user_giving_dates"(
+    "id" SERIAL PRIMARY KEY,
+    "user_id" SERIAL REFERENCES users(id) NOT NULL,
+    "date" date             NOT NULL,
+    "amount" DECIMAL        NOT NULL,
+    "location" VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE "sessions"(
+    "id" uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    "uid" SERIAL REFERENCES users(id) NOT NULL,
+    "timestamp" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+
