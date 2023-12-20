23447fdc91a28e10a8d837bb58f3fd44477e2020
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2023-12-20 01:17:58 +0100

    💄 Updated views
    * Changed background
    * Rebuilt profile page
    * Added Changing view for navigation bar (for logged and non-logged users)

--- app/main/views.py
+++ app/main/views.py
@@ -9,13 +9,13 @@ from ..auth.helpers import server_set_session, server_check_session, SESSION_NAM
 
 @main.route('/')
 def home():
-    return render_template('main.html')
+    return render_template('main.html', user_session=server_check_session())
 
 @main.route('/my_profile', methods=['GET', '[POST]'])
 def my_profile():
     # if user is logged in - show its profile data
     if server_check_session():        
         user = User.query.where(User.id == Session.query.where(Session.id == session[SESSION_NAME]['id']).scalar().uid).scalar()      
-        return render_template('profile.html', user = user)
+        return render_template('profile.html', user = user, user_session=True)
     # otherwise - redirect to login page
     return redirect(url_for('auth.login'))
